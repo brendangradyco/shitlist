a1c62df8cdb73be3e46f053bf6662c9d877441f8
brendangradyco <brendangrady.co@gmail.com>
2026-04-20 09:02:07 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shitlist-v8';
+const CACHE = 'shitlist-v9';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
